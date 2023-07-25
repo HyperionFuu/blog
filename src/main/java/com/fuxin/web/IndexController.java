3a3bc78dc4bb9cddb92e9539e9859dce3184a58b
HyperionFuu <50319636+HyperionFuu@users.noreply.github.com>
2023-07-25 10:45:45 +0800

    pom.xml

---
+++ src/main/java/com/fuxin/web/IndexController.java
@@ -0,0 +1,20 @@
+package com.fuxin.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Description: IndexController
+ * Author: Fuu
+ * Date: 2023/7/25
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        int i = 9/0;
+        return "index";
+    }
+
+}
